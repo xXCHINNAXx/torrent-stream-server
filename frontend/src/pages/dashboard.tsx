264ce82fa4ec2c9e52fdf1e999891ffc1bc0d179
xXCHINNAXx <chinnabhai944@gmail.com>
2022-08-13 02:34:43 +0530

    Update dashboard.tsx

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -8,8 +8,8 @@ export default withBearer(() => {
     return (
         <Container className="mt-3 content">
             <UsageWidget />
-            <ActiveTorrentsWidget />
-            <LogsWidget />
+            {//<ActiveTorrentsWidget />}
+                {//<LogsWidget />}
         </Container>
     )
 })
